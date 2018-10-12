daf590fb52db24c0ca2d8beca5a02d6b5e88f8fb
Avery <cameroav@uregina.ca>
2018-10-12 12:09:20 -0600

    Initial node setting using ListElement, only one node

--- src/averycameron/LinkedList.java
+++ src/averycameron/LinkedList.java
@@ -4,7 +4,8 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListElement le = new ListElement();
+		le.setData(5);
 	}
 
 }
--- src/averycameron/ListElement.java
+++ src/averycameron/ListElement.java
@@ -8,5 +8,19 @@ package averycameron;
  *
  */
 public class ListElement {
+	private ListElement next;
+	private int data;
 	
+	public ListElement() {
+		this.data = 0;
+		this.next = null;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	public int getData() {
+		return this.data;
+	}
+
 }
